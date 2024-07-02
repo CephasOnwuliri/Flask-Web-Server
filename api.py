2917ac027784a6b8ec8a912d039501182a77875d
Cephas Onwuliri <75612841+CephasOnwuliri@users.noreply.github.com>
2024-07-02 17:57:36 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,3 @@
-Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> from flask import Flask, request, jsonify
 ... import requests
 ... from ipdata import ipdata
@@ -12,13 +10,14 @@ Type "help", "copyright", "credits" or "license()" for more information.
 ...     client_ip = request.remote_addr
 ...     
 ...     # Use ipdata to get location info
-...     ipdata_client = ipdata.IPData('your_api_key_here')
+...     ipdata_client = ipdata.IPData('8f81b3ee4b0d59aefb2ea7d7b0b569d9800e7944b2ecbd1396606c3e
+')
 ...     response = ipdata_client.lookup(client_ip)
 ...     
 ...     city = response.get('city', 'Unknown')
 ...     
 ...     # Get temperature data
-...     weather_api_key = 'your_weather_api_key_here'
+...     weather_api_key = '27e8308f9f2c4372e1904d96152d5229'
 ...     weather_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}&units=metric'
 ...     weather_response = requests.get(weather_url).json()
 ...     temperature = weather_response['main']['temp']
